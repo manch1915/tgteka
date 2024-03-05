e4581cff622c5a588b07a0d8f14029d24a14568a
levon <levon.10h.y@gmail.com>
2024-03-05 22:11:22 +0400

    Adjust schedule command for updating finished orders
    The Kernel.php file has been updated to incorporate the use of 'UpdateFinishedOrdersCommand' class in the scheduling of tasks. Instead of directly referencing the command string 'update:finished-orders', we are now using the class method to call this command and it is now set to run in the background on an hourly schedule. This adjustment enhances the code organization and ensures better resource management by running the command in the background.
    
    Took 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateFinishedOrdersCommand;
 use App\Jobs\FetchAllChannelStatisticsJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('update:finished-orders')->hourly();
+        $schedule->command(UpdateFinishedOrdersCommand::class)->hourly()->runInBackground();
         $schedule->job(new \App\Jobs\UpdateChannelRatingAndScore)->hourly();
         $schedule->job(new FetchAllChannelStatisticsJob)->daily();
     }

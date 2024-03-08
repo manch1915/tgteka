bc8f4bfcd26868cbd646f39b69b51eeac466ea92
levon <levon.10h.y@gmail.com>
2024-03-08 09:44:19 +0400

    Remove 'type' validation from Channel requests
    The 'type' validation was removed from both UpdateChannelRequest and StoreChannelRequest classes in the application. This change implies that 'type' is no longer a required parameter when storing or updating channels, thus potentially simplifying the interaction with the API.
    
    Took 20 minutes

--- app/Http/Requests/StoreChannelRequest.php
+++ app/Http/Requests/StoreChannelRequest.php
@@ -14,7 +14,6 @@ class StoreChannelRequest extends FormRequest
             'description'        => 'required|string|max:300',
             'subscribers_source' => 'required|string|max:300',
             'topic_id'           => 'required',
-            'type'               => 'required|in:channel,chat',
             'url'                => ['required', 'regex:/^@(?:[a-z0-9\_]+)$|^(https\:\/\/t\.me\/[a-zA-Z0-9\_\+\-]+)$/i', 'unique:channels,url'],
             'repeat_discount'    => 'nullable|in:0,10,20,30,50',
             'male_percentage'    => 'integer|required',

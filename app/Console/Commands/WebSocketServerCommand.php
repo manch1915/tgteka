ac721397f1005604ec27e4d37dd314dbb7be7b7f
levon <levon.10h.y@gmail.com>
2024-04-27 23:20:54 +0400

    last fixing
    Took 6 minutes

--- app/Console/Commands/WebSocketServerCommand.php
+++ app/Console/Commands/WebSocketServerCommand.php
@@ -7,13 +7,15 @@ use App\Http\Factories\MessageFactory;
 use App\Http\Repositories\PersonalChatRepository;
 use App\Http\Repositories\SupportChatRepository;
 use Illuminate\Console\Command;
-use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Loop;
+use React\Socket\SecureServer;
+use React\Socket\SocketServer;
 
 class WebSocketServerCommand extends Command
 {
-    private const PORT = 1915;
     protected $signature = 'websocket:start';
     protected $description = 'Start the WebSocket Server';
 
@@ -36,14 +38,26 @@ class WebSocketServerCommand extends Command
 
     private function createChatServer(): IoServer
     {
-        return IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    new ChatWebSocketServer($this->personalChatRepository, $this->supportChatRepository, $this->messageFactory)
-                )
-            ),
-            self::PORT
+        $loop   = Loop::get();
+
+        $webSock = new SocketServer('0.0.0.0:1915',[], $loop);
+
+        $webSock = new SecureServer(
+            $webSock,
+            $loop,
+            array(
+                'local_cert'        => config('websockets.ssl.local_cert'),
+                'local_pk'          => config('websockets.ssl.local_pk'),
+                'allow_self_signed' => config('websockets.ssl.allow_self_signed'),
+                'verify_peer' => config('websockets.ssl.verify_peer')
+            )
         );
+
+        $webServer = new HttpServer(new WsServer(
+            new ChatWebSocketServer($this->personalChatRepository, $this->supportChatRepository, $this->messageFactory)
+        ));
+
+        return new IoServer($webServer, $webSock, $loop);
     }
 
     private function startServer(IoServer $server): void
@@ -51,4 +65,3 @@ class WebSocketServerCommand extends Command
         $server->run();
     }
 }
-
--- resources/js/Pages/Dashboard/Support.vue
+++ resources/js/Pages/Dashboard/Support.vue
@@ -31,7 +31,7 @@ const message = useMessage();
 const userId = computed(() => page.props.auth.user.id);
 
 const socket = new WebSocket(
-    `ws://${import.meta.env.VITE_APP_WEBSOCKETS_IP}:1915/?userid=${
+    `wss://${import.meta.env.VITE_APP_WEBSOCKETS_IP}:1915/?userid=${
         userId.value
     }`
 );
